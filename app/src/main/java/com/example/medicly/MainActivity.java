eacad48dacee32d8ab3d5811c14f2f48abaee9a0
Aaron <aakiswa@gmail.com>
2022-10-21 13:51:49 +0800

    7th Commit - Aaron

--- app/src/main/java/com/example/medicly/MainActivity.java
+++ app/src/main/java/com/example/medicly/MainActivity.java
@@ -11,15 +11,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.doc_prof_screen);
+        setContentView(R.layout.health_records);
 
-        /**
         Spinner mySpinner = (Spinner) findViewById(R.id.sexSpinner);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.sex));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
-         **/
 
     }
 }
