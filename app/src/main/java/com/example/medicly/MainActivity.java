81b73a0bd16b396065556b479f8032526b32005c
Aaron <aakiswa@gmail.com>
2022-10-25 22:43:54 +0800

    11th Commit - Aaron

--- app/src/main/java/com/example/medicly/MainActivity.java
+++ app/src/main/java/com/example/medicly/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.health_records);
+        setContentView(R.layout.appointment_scheduling);
 
         /**
         Spinner mySpinner = (Spinner) findViewById(R.id.sexSpinner);
