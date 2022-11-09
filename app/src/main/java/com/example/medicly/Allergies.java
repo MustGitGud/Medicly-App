080df8aa85014c0f469020b20e3693d3ea8baf4f
Aaron <aakiswa@gmail.com>
2022-11-09 19:35:06 +0800

    25th Commit - Aaron

--- app/src/main/java/com/example/medicly/Allergies.java
+++ app/src/main/java/com/example/medicly/Allergies.java
@@ -1,18 +1,31 @@
 package com.example.medicly;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.google.firebase.database.Query;
 
 public class Allergies extends AppCompatActivity {
+    LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.allergies);
+        container = findViewById(R.id.linLayAllergy);
 
         ImageButton bckBtn = findViewById(R.id.bckBtnAllergy);
         bckBtn.setOnClickListener(new View.OnClickListener(){
@@ -22,5 +35,24 @@ public class Allergies extends AppCompatActivity {
             }
         });
 
+        ImageButton addBtn = findViewById(R.id.addBtnAllergy);
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                addContainer();
+            }
+        });
     }
+
+    public void addContainer() {
+        EditText editText = new EditText(this);
+        container.addView(editText);
+        editText.setGravity(Gravity.LEFT);
+        editText.setMinHeight(300);
+        editText.setBackgroundResource(R.drawable.edit_txt_bg);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) editText.getLayoutParams();
+        layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        layoutParams.bottomMargin = 20;
+        editText.setLayoutParams(layoutParams);
+    }
+
 }
