1d279287c6adde55ffd7085696a1f6c9ee7eace8
Aaron <aakiswa@gmail.com>
2022-11-16 20:59:12 +0800

    31th commit - Aaron

--- app/src/main/java/com/example/medicly/Allergies.java
+++ app/src/main/java/com/example/medicly/Allergies.java
@@ -3,6 +3,7 @@ package com.example.medicly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
@@ -38,7 +39,7 @@ public class Allergies extends AppCompatActivity {
             }
         });
 
-        ImageButton addBtn = findViewById(R.id.addBtnAllergy);
+        Button addBtn = findViewById(R.id.addBtnAllergy);
         addBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(Allergies.this, AllergiesEditor.class));
